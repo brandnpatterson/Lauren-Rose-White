2a322fa9e53c1c0f6ac25b2e705e5c57bdb35ec5
Brandon Patterson <brandonnpatterson@gmail.com>
2016-08-22 16:14:48 -0500

    completed nav for desktop JS

--- app/js/nav-close.js
+++ app/js/nav-close.js
@@ -4,15 +4,18 @@
   const navLeft = document.getElementById('nav-left')
 
   navClose.onclick = () => {
-
-    return navLeft()
-  }
-
-  navLeft.onclick = () => {
     let x = document.getElementById('nav-left')
     if (x.className === 'nav-left') {
-      x.className += 'responsive'
+      x.className = 'responsive'
     } else {
+      x.className = 'nav-left'
+    }
+  }
+
+  // If nav-left is clicked and has the class of responsive, change it's class to to nav-left
+  navLeft.onclick = () => {
+    let x = document.getElementById('nav-left')
+    if (x.className === 'responsive') {
       x.className = 'nav-left'
     }
   }
