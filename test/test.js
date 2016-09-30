79a11469f989078cc7d6c3b37e24971254aa30af
Brandon Patterson <brandonnpatterson@gmail.com>
2016-09-30 12:09:31 -0500

    test 2

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ require('chai').should()
 describe('Two plus two', function () {
   it('should test that 2 + 2 = 4', function () {
     var value = 2 + 2
-    value.should.equal(3)
+    value.should.equal(4)
   })
 })
 
