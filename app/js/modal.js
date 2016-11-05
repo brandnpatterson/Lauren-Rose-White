68c02a726be09ecc96629a84ca0df0fffb2f60ed
Brandon Patterson <brandonnpatterson@gmail.com>
2016-11-05 14:18:12 -0500

    reordered comments

--- app/js/modal.js
+++ app/js/modal.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
 
-  //    Close all modals    //
-  $(document).on('click', '.closebtn', function(){
+
+  $(document).on('click', '.closebtn', function(){ // Close all modals
     $('#modal01, #modal02, #modal03, #modal04, #modal05, #modal06, #modal06, #modal07, #modal08, #modal09, #modal10, #modal11, #modal12, #modal13, #modal14, #modal15, #modal16, #modal 17, #modal18, #modal19, #modal20').hide()
   })
 
-  //    Open Modal - Home #img01-03   //
-  $(document).on('click', '.get-modal', function(){
+
+  $(document).on('click', '.get-modal', function(){ // Open Modal depending on which data-click is clicked
     var $this = this
     var modalId = $($this).attr('data-click')
     $('#'+modalId).toggle()
