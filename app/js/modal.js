c7ad4f07d618a3213047ebd8ccd20327647edd9e
Brandon Patterson <brandonnpatterson@gmail.com>
2016-11-20 19:56:04 -0600

    completely rewrote modal CSS with simple pixels instead of %

--- app/js/modal.js
+++ app/js/modal.js
@@ -12,12 +12,12 @@ var Modal = (function() {
   $document.on('click', '.closebtn', closeBtn);
 
   function closeBtn() { // When .closebtn is clicked, close all modals
-    $('.modal').hide();
+    $('.modal').fadeOut();
   }
 
   function getModal() { // Open Modal depending on which data-click is clicked
     var $this = this;
     var modalId = $($this).attr('data-click');
-    $('#'+modalId).toggle();
+    $('#'+modalId).fadeIn();
   }
 })();
